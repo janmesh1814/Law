1f12601c23db12f8deb926fda79e0da4923dbfc7
janmesh rajput <janmeshrajput1814@gmail.com>
2024-04-05 00:16:20 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginPage from './mycomponent/login';
+
+function App() {
+    return ( < >
+        <
+        LoginPage / >
+        <
+        />
+    );
+}
+
+export default App;
