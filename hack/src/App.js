4dfc6de16702dccbf3a0d880e641ed87f41a6e30
manasagar <123059765+manasagar@users.noreply.github.com>
2024-04-05 22:00:23 +0530

    added basic routes

--- hack/src/App.js
+++ hack/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LoginPage from './mycomponent/login';
+import Home from './mycomponent/home';
+import LoginProf from './mycomponent/loginprof';
 
-import Register from './mycomponent/register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginUsr from './mycomponent/loginuser';
+
 function App() {
   return (
     <>
-    <LoginPage/>
+<BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="page1" element={<LoginUsr/>}></Route>
+          <Route exact path="page2" element={<LoginProf/>}></Route>
+          
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
