9c18eded9467e2da389838c9d1165d6945c0f866
manasagar <123059765+manasagar@users.noreply.github.com>
2024-04-05 23:19:05 +0530

    done

--- hack/src/App.js
+++ hack/src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './mycomponent/home';
 import LoginProf from './mycomponent/loginprof';
-
+import Register from './mycomponent/register';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginUsr from './mycomponent/loginuser';
-
+import LoginPage from './mycomponent/login';
 function App() {
   return (
     <>
@@ -14,7 +14,8 @@ function App() {
         <Route exact path="/" element={<Home />}></Route>
           <Route exact path="page1" element={<LoginUsr/>}></Route>
           <Route exact path="page2" element={<LoginProf/>}></Route>
-          
+          <Route exact path='page1/Signupusr' element={<Register/>}></Route>
+          <Route exact path='page2/Signupprof' element={<LoginPage/>}></Route>
       </Routes>
     </BrowserRouter>
     </>
